8115bbd7443130de23321d6b8c7553e4dc6f9c4d
tainguyen1610 <nguyentaitan161005@gmail.com>
2026-07-12 11:12:18 +0700

    Phân tích dữ liệu và trường hợp biên của thêm sản phẩm
    Checklist yêu cầu và trường hợp biên của thêm sản phẩm

---
+++ SSessionCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniSmartstoreMvc.ViewModels
+{
+    public class SessionCartItem
+    {
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+        public string? SelectedColor { get; set; }
+
+    }
+}
