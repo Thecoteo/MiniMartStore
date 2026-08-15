0019d9c2a92dc52b5a6003d159cc6e7772e85aab
tainguyen1610 <nguyentaitan161005@gmail.com>
2026-08-15 14:36:18 +0700

    Xây dựng xử lý chính cho bật hoặc tắt khuyến mãi
    Kết quả bật hoặc tắt khuyến mãi được hoàn thiện theo Acceptance Criteria

---
+++ ShopDrawerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MiniSmartstoreMvc.Models;
+
+namespace MiniSmartstoreMvc.ViewModels
+{
+    public class ShopDrawerViewModel
+    {
+        public string ActiveTab { get; set; } = "cart";
+
+        public List<CartItemViewModel> CartItems { get; set; } = new();
+
+        public List<Product> WishlistProducts { get; set; } = new();
+
+        public List<Product> CompareProducts { get; set; } = new();
+    }
+}
