f85e5b21c636c0558691bc2ab4c2113148c24020
tainguyen1610 <nguyentaitan161005@gmail.com>
2026-08-29 14:36:33 +0700

    Xây dựng chức năng và tích hợp dữ liệu cho hủy đơn hàng
    Kết quả hủy đơn hàng được hoàn thiện theo Acceptance Criteria

---
+++ OrderDetail.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MiniSmartstoreMvc.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+
+        public Order? Order { get; set; }
+
+        public int ProductId { get; set; }
+
+        public Product? Product { get; set; }
+
+        public int Quantity { get; set; }
+        public string? SelectedColor { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal UnitPrice { get; set; }
+
+        [NotMapped]
+        public decimal TotalPrice => UnitPrice * Quantity;
+    }
+}
