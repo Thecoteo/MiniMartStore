fba4c947e0fee71e4559267c9de304d3fd07237d
NguyenVanHai77 <Nguyenvanhai38679@gmail.com>
2026-09-01 01:30:29 +0700

    Xây dựng xử lý chính cho xem trạng thái thanh toán
    Kết quả xem trạng thái thanh toán được hoàn thiện theo Acceptance Criteria

---
+++ Payment.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MiniSmartstoreMvc.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+
+        public Order? Order { get; set; }
+
+        public PaymentMethod PaymentMethod { get; set; }
+
+        public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.Pending;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+
+        public string? TransactionCode { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
